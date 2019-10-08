fc5e38030af2fa2fb45031d0ea9333f3184da23b
Michael R.F <mrflowers22@uwalumni.com>
2019-10-08 09:05:17 -0600

    initial  commit

---
+++ JournalOBJC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JournalOBJC
+//
+//  Created by Michael Flowers on 10/8/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JournalOBJC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JournalOBJC
+//
+//  Created by Michael Flowers on 10/8/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
